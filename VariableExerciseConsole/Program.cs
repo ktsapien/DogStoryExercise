c80feee625147e4b8d2dec5b32f5c521fa2a9e35
ktsapien <kelseysapien@gmail.com>
2021-10-11 13:41:59 -0500

    initial commit

---
+++ VariableExerciseConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariableExerciseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Dante";
+            int dogAge = 7;
+            char exclamation = '!';
+            bool lovesNapping = true;
+            double kissesPerDay = 80;
+            decimal weight = 44.5m;
+
+
+            Console.WriteLine($"My dog's name is {dogName}, she is {dogAge} years old{exclamation} She weighs {weight}lbs It is {lovesNapping} she loves napping. She gets {kissesPerDay} kisses per day");
+        }
+    }
+}
